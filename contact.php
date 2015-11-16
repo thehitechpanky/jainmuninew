a062ede4fbc63239476b67764cada0cac7037cc0
Pankaj Jain <capankajjain@smilyo.com>
2015-11-16 23:42:27 +0530

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,38 @@
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=____________";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<!----start-contact---->
+<div id="contact" class="contact">
+	<div class="contact-map">
+		<iframe></iframe>
+	</div>
+	<div class="contact-grids">
+		<div class="col-md-6 contact-left">
+			<h3><span> </span> Contact</h3>
+			<p><i class="fa fa-map-marker"> </i> Pankaj Jain | +91-9818609955 | capankajjain@smilyo.com</p>
+			<form>
+				<input type="text" value="Name " onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Name';}">
+				<input type="text" value="info@mail.com *" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Email@domain.com *';}">
+				<textarea onfocus="if(this.value == 'Message *') this.value='';" onblur="if(this.value == '') this.value='Message *;">Message *</textarea>
+				<p class="conditions"> <label><span>*</span>Fields in * are mandatory.</label></p>
+				<span class="submit-btn"><input type="submit" value="Send"></span>
+			</form>
+		</div>
+		<div class="col-md-6 contact-right">
+			<div class="fb-page" data-href="https://www.facebook.com/jainmunilocator" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="true">
+				<div class="fb-xfbml-parse-ignore">
+					<blockquote cite="https://www.facebook.com/jainmunilocator">
+						<a href="https://www.facebook.com/jainmunilocator">Jain Muni Locator</a>
+					</blockquote>
+				</div>
+			</div>
+		</div>
+		<div class="clearfix"> </div>
+	</div>
+</div>
+<!----//End-contact---->
