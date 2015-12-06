f962b63fae5d70c6f965628c2c125a940f05231e
Pankaj Jain <capankajjain@smilyo.com>
2015-12-06 08:38:23 +0530

    Create foot.php

---
+++ foot.php
@@ -0,0 +1,26 @@
+<!----start-footer---->
+<div class="footer">
+	<div class="container">
+		<div class="footer-left">
+			<a href="#"><img src="images/footer-logo.png" title="Jain Muni Locator" /></a>
+			<!--<p>Template by <a href="http://w3layouts.com/">W3layouts</a></p>-->
+		</div>
+		<script type="text/javascript">
+			$(document).ready(function() {
+				/*
+					var defaults = {
+			  			containerID: 'toTop', // fading element id
+						containerHoverID: 'toTopHover', // fading element hover id
+						scrollSpeed: 1200,
+						easingType: 'linear' 
+			 		};
+					*/
+						
+						$().UItoTop({ easingType: 'easeOutQuart' });
+						
+					});
+				</script>
+				<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+			</div>
+</div>
+<!----//End-footer---->
