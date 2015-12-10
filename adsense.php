5c04c1aaf772059dc56075430aacd8bfadec1ecb
Pankaj Jain <capankajjain@smilyo.com>
2015-12-10 08:31:07 +0530

    Create adsense.php

---
+++ adsense.php
@@ -0,0 +1,10 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Jainism -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="_______"
+     data-ad-slot="________"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
