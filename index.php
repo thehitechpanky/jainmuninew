a5f0b6201ed39d7b7249a48ddd148ad8434c1c3a
Pankaj Jain <capankajjain@smilyo.com>
2015-12-10 08:34:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -227,36 +227,7 @@ $title = 'Jain Muni Locator | Home :: online destination for digambara jains';
 		</div>
 		<div class="clearfix"> </div>
 		<!----//End-reads----->
-		<?php
-include 'contact.php';
-include 'adsense.php';
-		?>
-		<!----start-footer---->
-		<div class="footer">
-			<div class="container">
-				<div class="footer-left">
-					<a href="#"><img src="images/footer-logo.png" title="Jain Muni Locator" /></a>
-					<!--<p>Template by <a href="http://w3layouts.com/">W3layouts</a></p>-->
-				</div>
-				<script type="text/javascript">
-					$(document).ready(function() {
-						/*
-					var defaults = {
-			  			containerID: 'toTop', // fading element id
-						containerHoverID: 'toTopHover', // fading element hover id
-						scrollSpeed: 1200,
-						easingType: 'linear' 
-			 		};
-					*/
-					
-					$().UItoTop({ easingType: 'easeOutQuart' });
-					
-				});
-				</script>
-				<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
-			</div>
-	</div>
-	<!----//End-footer---->
-	<!----//End-container---->
+		<?php include 'foot.php'; ?>
+		<!----//End-container---->
 	</body>
 </html>
