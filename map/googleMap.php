e1db68c386f4bfab0d608dd54b597647c2222404
Pankaj Jain <capankajjain@smilyo.com>
2015-12-10 08:38:20 +0530

    Create googleMap.php

---
+++ map/googleMap.php
@@ -0,0 +1,2 @@
+<script type="text/javascript" src="map/googleMap.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=______________________&callback=initMap" async defer></script>
