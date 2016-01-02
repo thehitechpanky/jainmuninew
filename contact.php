4c247db1b27ce5618c840c3b0fe4d078d9ca18b0
Pankaj Jain <capankajjain@smilyo.com>
2016-01-02 16:13:28 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,12 +1,12 @@
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=__________";
-  fjs.parentNode.insertBefore(js, fjs);
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=1382746718688949";
+	fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
-<!----start-contact---->
+<!--start-contact-->
 <div id="contact" class="contact">
 	<div class="contact-map">
 		<iframe></iframe>
@@ -35,4 +35,4 @@
 		<div class="clearfix"> </div>
 	</div>
 </div>
-<!----//End-contact---->
+<!--//End-contact-->
