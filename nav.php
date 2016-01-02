909696db433114b02964008771f68d72e50b29ef
Pankaj Jain <capankajjain@smilyo.com>
2016-01-02 15:39:09 +0530

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,13 @@
+<!--start-logo-->
+<div class="logo">
+	<a href="index.php"><img src="images/logo.png" alt="Jain Muni Locator" title="Jain Muni Locator" /></a>
+</div>
+<!--End-logo-->
+<!--start-top-nav-->
+<nav class="top-nav">
+	<ul class="top-nav">
+		<?php echo $navlinks; ?>
+	</ul>
+	<a href="#" id="pull"><img src="images/nav-icon.png" alt="menu" title="menu" /></a>
+</nav>
+<div class="clearfix"> </div>
