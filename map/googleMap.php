e6b359726b44a9097390ace61697887b7cbe21ce
Pankaj Jain <capankajjain@smilyo.com>
2016-03-12 13:57:40 +0530

    Update googleMap.php

--- map/googleMap.php
+++ map/googleMap.php
@@ -1,2 +1,14 @@
-<script type="text/javascript" src="map/googleMap.js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=______________________&callback=initMap" async defer></script>
+<html lang="en">
+	<head>
+		<meta name="google-signin-scope" content="profile email">
+		<meta name="google-signin-client_id" content="______________">
+		<script src="map/googleMap.js" type="text/javascript"></script>
+		<script src="https://maps.googleapis.com/maps/api/js?key=____________&libraries=places&callback=initMap"></script>
+		<script src="map/sliderMenu.js" type="text/javascript"></script>
+		<script src="https://apis.google.com/js/platform.js" async defer></script>
+	</head>
+	<body>
+		<div class="g-signin2" data-onsuccess="onSignIn" style="float: right;"></div>
+		<script src="map/signIn.js" type="text/javascript"></script>
+	</body>
+</html>
