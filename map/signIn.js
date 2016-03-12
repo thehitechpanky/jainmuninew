8d6d8fc8a1d6a32b74338c2ab0b640d97aaad56a
Pankaj Jain <capankajjain@smilyo.com>
2016-03-12 13:56:46 +0530

    Create signIn.js

---
+++ map/signIn.js
@@ -0,0 +1,20 @@
+//navigator.geolocation.getCurrentPosition(function(position) {
+//	var userlat = position.coords.latitude;
+//	var userlng = position.coords.longitude;
+//});				
+function onSignIn(googleUser) {
+	// Useful data for your client-side scripts:
+	var profile = googleUser.getBasicProfile();
+	
+	// The ID token you need to pass to your backend:
+	var id_token = googleUser.getAuthResponse().id_token;
+	var username = profile.getName();
+	var email = profile.getEmail();			
+	if (window.XMLHttpRequest) {
+		xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("GET", "map/updateLocation.php?email=" + email + "&username=" + username , true);
+		xmlhttp.send();
+		return false;
+		alert("Hello");
+	}
+};
