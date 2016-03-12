0598f04e98fa2034ded65ccacc3904ff2c9b0ab3
Pankaj Jain <capankajjain@smilyo.com>
2016-03-12 14:37:51 +0530

    Update updateLocation.php

--- map/updateLocation.php
+++ map/updateLocation.php
@@ -7,15 +7,19 @@ $userlat = 0;
 $userlng = 0;
 $userlocation = "N/A";
 
+// fileds of editlog
+$usertimestamp = date("Y-m-d H:i:s", time());
+$userip = $_SERVER['REMOTE_ADDR'];
+
 $u = $db->prepare("SELECT * FROM user WHERE email=?");
 $u->execute(array($email));
 
 if($u->rowCount() == 1) {
-	$q = $db->prepare("UPDATE user SET username=? WHERE email=?");
-	$q->execute(array($username,$email));
+	$q = $db->prepare("UPDATE user SET username=?, usertimestamp=?, userip WHERE email=?");
+	$q->execute(array($username,$usertimestamp,$userip,$email));
 } else {
-	$q = $db->prepare("INSERT INTO user (email,username,userlat,userlng,userlocation) VALUES (?,?,?,?,?)");
-	$q->execute(array($email,$username,$userlat,$userlng,$userlocation));
+	$q = $db->prepare("INSERT INTO user (email,username,userlat,userlng,userlocation,usertimestamp,userip) VALUES (?,?,?,?,?,?,?)");
+	$q->execute(array($email,$username,$userlat,$userlng,$userlocation,$usertimestamp,$userip));
 }
 
 ?>
