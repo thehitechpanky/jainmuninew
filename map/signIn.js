afd9c4bdd841899666c914b7ad68de0a6cb3c854
Pankaj Jain <capankajjain@smilyo.com>
2016-03-12 16:35:33 +0530

    Update signIn.js

--- map/signIn.js
+++ map/signIn.js
@@ -9,12 +9,13 @@ function onSignIn(googleUser) {
 	// The ID token you need to pass to your backend:
 	var id_token = googleUser.getAuthResponse().id_token;
 	var username = profile.getName();
-	var email = profile.getEmail();			
+	var email = profile.getEmail();	
+	var userimg = profile.getImageUrl();
 	if (window.XMLHttpRequest) {
 		xmlhttp = new XMLHttpRequest();
-		xmlhttp.open("GET", "map/updateLocation.php?email=" + email + "&username=" + username , true);
+		xmlhttp.open("GET", "map/updateLocation.php?email=" + email + "&username=" + username + "&userimg=" + userimg , true);
 		xmlhttp.send();
 		return false;
-		alert("Hello");
+		//alert("Hello");
 	}
 };
